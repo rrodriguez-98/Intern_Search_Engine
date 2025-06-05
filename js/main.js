913692c2b8c48bf4f2e5ebb3b7687cf72c8d9a3c
rrodriguez-98 <rebecca.alia.rodriguez@gmail.com>
2025-06-05 13:41:59 -0700

    Add comments to document code.

--- js/main.js
+++ js/main.js
@@ -1,19 +1,32 @@
+//-----------------Autopopulate Search Keywords with Buttons------------------//
+$("#try_swe").click(function(string){
+    document.getElementById("search-keyword").value = 'software engineering';
+})
 
+$("#try_market").click(function(string){
+    document.getElementById("search-keyword").value = 'marketing';
+})
+
+$("#try_danalyst").click(function(string){
+    document.getElementById("search-keyword").value = 'data analyst';
+})
+//-----------------Function: Remove Spaces From Keyword-------------------------//
 function removeSpaces(string) {
     return string.replace(' ', '%20')
 }
-
+//-------------------Initially Hide Loading Spinner------------------------------//
 $("#loading-spinner").hide();
 
+
+//------------Function: List Job Results Upon Button Click-----------------------//
 $("#job-search-btn").click(function(){
     //Empty contents before starting another search
     $("#job-list-header").empty();
     $("#job-list").empty();
-
+    //Obtain user input
     let keyword = $("#search-keyword").val();
     let title_filter = removeSpaces(keyword);
-    
-
+    //API Settings
     const settings = {
         async: true,
         crossDomain: true,
@@ -24,31 +37,31 @@ $("#job-search-btn").click(function(){
             'x-rapidapi-host': 'internships-api.p.rapidapi.com'
         }
     };
-
+    //Check if user input is empty
     if (title_filter !== ""){
         $("#loading-spinner").show();
-        $.ajax(settings).done(function (response) {
-            
-        if (response.length!== 0){
-            $("#loading-spinner").hide();
-            $("#search-keyword").val('');
-            $("#job-list-header").append(`<p style="color: white">Top Results:</p>`)
-            for (let i = 0; i < response.length; i++) {
-                if (response[i].organization_logo === null){
-                    response[i].organization_logo = './empty_logo.png';
+        $.ajax(settings).done(function (response) { 
+            if (response.length!== 0){
+                $("#loading-spinner").hide();
+                $("#search-keyword").val('');
+                $("#job-list-header").append(`<p style="color: white;">Top Results:</p>`)
+                for (let i = 0; i < response.length; i++) {
+                    if (response[i].organization_logo === null){
+                        response[i].organization_logo = './empty_logo.png';
+                    }
+                    //Render job listings
+                    $("#job-list").append(`
+                        <a href="${response[i].url}" style="color: black;">
+                        <div class="card" style="background-color: white; padding: 5%; border-radius: 10px">
+                            <img src="${response[i].organization_logo}" alt="Company Logo">
+                            <h3>${response[i].organization}</h3>
+                            <h4>${response[i].title}</h4>
+                        </div>
+                        </a>
+                    `);
                 }
-                
-                $("#job-list").append(`
-                    <a href="${response[i].url}" style="color: black;">
-                    <div class="card" style="background-color: white; padding: 5%; border-radius: 10px">
-                        <img src="${response[i].organization_logo}" alt="Company Logo">
-                        <h3>${response[i].organization}</h3>
-                        <h4>${response[i].title}</h4>
-                    </div>
-                    </a>
-                `);
-            }
         } else{
+            $("#loading-spinner").hide();
             $("#job-list-header").append(`<p style="color: white">No results. Try another search.</p>`)
         }
         console.log(response);
@@ -56,7 +69,6 @@ $("#job-search-btn").click(function(){
     } else {
         $("#job-list-header").append(`<p style="color: white;">Please search for a position title.</p>`)
     }
-    
 
 });
 
