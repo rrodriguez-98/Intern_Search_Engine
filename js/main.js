7c7feece033d39a4196a93c22b92ebc58e924f5c
rrodriguez-98 <rebecca.alia.rodriguez@gmail.com>
2025-06-04 19:51:06 -0700

    Able to search keyword for position title. Add loading icon to display during wait for data.

--- js/main.js
+++ js/main.js
@@ -1,29 +1,57 @@
-const settings = {
-	async: true,
-	crossDomain: true,
-	url: 'https://internships-api.p.rapidapi.com/active-jb-7d',
-	method: 'GET',
-	headers: {
-		'x-rapidapi-key': 'f473df81ccmsh788d696e77ff01bp17c473jsn1ee70510cce3',
-		'x-rapidapi-host': 'internships-api.p.rapidapi.com'
-	}
-};
+
+function removeSpaces(string) {
+    return string.replace(' ', '%20')
+}
+
+$("#loading-spinner").hide();
 
 $("#job-search-btn").click(function(){
-    $.ajax(settings).done(function (response) {
-        $("#job-list-header").append(`<p style="color: white">Top Results:</p>`)
-        for (let i = 0; i < response.length; i++) {
-            
-            $("#job-list").append(`
-                <div class="card" style="background-color: white; padding: 5%; border-radius: 10px">
-                    <img src="${response[i].organization_logo}" alt="Company Logo">
-                    <h3>${response[i].organization}</h3>
-                    <h4>${response[i].title}</h4>
-                </div>
-            `);
-        }
+    //Empty contents before starting another search
+    $("#job-list-header").empty();
+    $("#job-list").empty();
 
+    let keyword = $("#search-keyword").val();
+    let title_filter = removeSpaces(keyword);
+
+    const settings = {
+        async: true,
+        crossDomain: true,
+        url: `https://internships-api.p.rapidapi.com/active-jb-7d?title_filter=${title_filter}`,
+        method: 'GET',
+        headers: {
+            'x-rapidapi-key': 'f473df81ccmsh788d696e77ff01bp17c473jsn1ee70510cce3',
+            'x-rapidapi-host': 'internships-api.p.rapidapi.com'
+        }
+    };
+
+    if (title_filter !== ""){
+        $("#loading-spinner").show();
+        $.ajax(settings).done(function (response) {
+            
+        if (response.length!== 0){
+            $("#loading-spinner").hide();
+            $("#job-list-header").append(`<p style="color: white">Top Results:</p>`)
+            for (let i = 0; i < response.length; i++) {
+                
+                $("#job-list").append(`
+                    <a href="${response[i].url}" style="color: black;">
+                    <div class="card" style="background-color: white; padding: 5%; border-radius: 10px">
+                        <img src="${response[i].organization_logo}" alt="Company Logo">
+                        <h3>${response[i].organization}</h3>
+                        <h4>${response[i].title}</h4>
+                    </div>
+                    </a>
+                `);
+            }
+        } else{
+            $("#job-list-header").append(`<p style="color: white">No results. Try another search.</p>`)
+        }
         console.log(response);
-    })
+        })
+    } else {
+        $("#job-list-header").append(`<p style="color: white">Please search for a position title.</p>`)
+    }
+    
+
 });
 
